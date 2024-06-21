3f56a300d940dcc6b30c21f718675350d385f7ad
Felipe-33 <fepepiuerj@gmail.com>
2024-06-21 16:39:35 -0300

    Testes do fluxo Teams

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -2,44 +2,30 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-
+import TeamModel from '../database/models/teams.model';
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
-
-import { Response } from 'superagent';
+import { teamMock1, AllTeams } from './mocks/teamsMock';
+import { Model } from 'sequelize';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('Testa o fluxo Teams', () => {
 
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  it('Testa o getAll', async () => {
+    sinon.stub(TeamModel, 'findAll').resolves(AllTeams as unknown as Model[]);
+    const res = await chai.request(app).get('/teams');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.deep.equal(AllTeams);
   });
+
+  it('Testa o getById', async () => {
+    sinon.stub(TeamModel, 'findByPk').resolves(teamMock1 as unknown as Model);
+    const res = await chai.request(app).get('/teams/1');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.deep.equal(teamMock1);
+  });
+
+  afterEach(sinon.restore);
 });
---
+++ app/backend/src/tests/mocks/teamsMock.ts
@@ -0,0 +1,19 @@
+export const AllTeams = [
+    {
+      id: 1,
+      teamName: "Avaí/Kindermann"
+    },
+    {
+      id: 2,
+      teamName: "Bahia"
+    },
+    {
+      id: 3,
+      teamName: "Botafogo"
+    },
+  ];
+
+  export const teamMock1 = {
+    id: 1,
+    teamName: 'Avaí/Kindermann',
+  }
